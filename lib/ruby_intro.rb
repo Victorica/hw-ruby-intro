c208c0a1fca22379e44bdbfff7af6c8fb9ed6f4b
C9Rubbish <2468202720@qq.com>
2016-04-26 13:12:10 +0000

    ok

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -9,11 +9,50 @@ return sum
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+   
+   large=0
+   large2=0
+   count=2
+   if arr.length==0 then
+       return 0
+    elsif arr.length==1 then
+       return arr.at(0)
+   else
+       large=(arr.at(0)>arr.at(1))?arr.at(0):arr.at(1)
+       large2=(arr.at(0)>arr.at(1))?arr.at(1):arr.at(0)
+   end
+       
+   while count<arr.length do
+     
+       if arr.at(count)>large then
+         large2=large
+         large=arr.at(count)
+        elsif arr.at(count)>large2 then
+         large2=arr.at(count)
+        
+       end 
+       
+     count=count+1
+   end
+   
+   return large+large2  
+   
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+ if arr.length==0 || arr.length==1 then
+     return false
+ end
+ 
+ for i in 0..(arr.length-1) 
+    for j in (i+1)..(arr.length-1)
+    if arr.at(i)+arr.at(j)==n then
+        return true
+    end
+    end
+end
+return false
+ 
 end
 
 # Part 2
